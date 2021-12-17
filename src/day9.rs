e27dad14facfdbfe32354a6920b3e2c9b52dd66b
booooh <ben.dayan@gmail.com>
2021-12-17 11:25:50 +0200

    day9 part 1

--- src/day9.rs
+++ src/day9.rs
@@ -16,12 +16,82 @@ where
     Ok(BufReader::new(file).lines())
 }
 
+const SIZE: usize = 100;
+struct HeightMap {
+    map: Vec<u32>,
+}
+
+#[derive(Debug)]
+struct Location {
+    idx: usize,
+    height: u32,
+}
+
+impl Location {
+    fn is_low_point(&self, others: Vec<Location>) -> bool {
+        others.iter().all(|x| self.height < x.height)
+    }
+}
+
+impl HeightMap {
+    fn from_lines(lines: Lines<BufReader<File>>) -> Self {
+        let map: Vec<_> = lines
+            .map(|line| parse_numbers(&line.unwrap()))
+            .flat_map(|x| x)
+            .collect();
+        return Self { map };
+    }
+
+    fn get_neighbors(&self, idx: usize) -> Vec<Location> {
+        let mut res = Vec::new();
+        // not first row
+        if idx >= SIZE {
+            res.push(Location {
+                idx: idx - SIZE,
+                height: self.map[idx - SIZE],
+            })
+        }
+        // not first column
+        if idx % SIZE != 0 {
+            res.push(Location {
+                idx: idx - 1,
+                height: self.map[idx - 1],
+            })
+        }
+
+        // not last row
+        if idx < SIZE * (SIZE - 1) {
+            res.push(Location {
+                idx: idx + SIZE,
+                height: self.map[idx + SIZE],
+            })
+        }
+
+        // not last column
+        if idx % SIZE != SIZE - 1 {
+            res.push(Location {
+                idx: idx + 1,
+                height: self.map[idx + 1],
+            })
+        }
+
+        return res;
+    }
+}
+
 pub(crate) fn day9part1() -> usize {
     let mut lines = read_lines("input9.t").unwrap();
-    let input: Vec<_> = lines
-        .map(|line| parse_numbers(&line.unwrap()))
-        .flat_map(|x| x)
+    let m = HeightMap::from_lines(lines);
+    let low_points: Vec<_> = m
+        .map
+        .iter()
+        .enumerate()
+        .map(|(idx, height)| Location {
+            idx,
+            height: *height,
+        })
+        .filter(|loc| loc.is_low_point(m.get_neighbors(loc.idx)))
         .collect();
-    println!("{:?}", input);
-    return input.len();
+    println!("{:?}", low_points);
+    return low_points.len() + low_points.iter().map(|x| x.height as usize).sum::<usize>();
 }
