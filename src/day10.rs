981dde600da88ce006238729bc1ae6134a703291
booooh <ben.dayan@gmail.com>
2021-12-17 18:28:16 +0200

    day10 part2

--- src/day10.rs
+++ src/day10.rs
@@ -50,6 +50,32 @@ fn is_corrupt(line: &str) -> Option<char> {
     return None;
 }
 
+fn completion(line: &str) -> Vec<char> {
+    let mut char_stack = Vec::<char>::new();
+    for c in line.chars() {
+        if is_open_char(c) {
+            char_stack.push(c);
+        } else {
+            if matching_char(c) == char_stack.pop().unwrap() {
+                continue;
+            }
+        }
+    }
+    // stack now contains openings that need to be closed
+    let completion_stack = Vec::<char>::new();
+    return char_stack.iter().rev().map(|&c| matching_char(c)).collect();
+}
+
+fn completion_score(completion_stack: Vec<char>, score_map: &HashMap<char, usize>) -> usize {
+    let mut score: usize = 0;
+    for c in completion_stack {
+        score = score * 5;
+        score += score_map.get(&c).unwrap();
+    }
+
+    return score;
+}
+
 pub(crate) fn day10part1() -> i32 {
     let mut lines = read_lines("input10.t").unwrap();
     // let corrupt_chars = Vec::<char>::new();
@@ -68,4 +94,23 @@ pub(crate) fn day10part1() -> i32 {
         }
     }
     return score;
+}
+
+pub(crate) fn day10part2() -> usize {
+    let mut lines = read_lines("input10.t").unwrap();
+    // let corrupt_chars = Vec::<char>::new();
+    let mut score_map = HashMap::<char, usize>::new();
+    score_map.insert(')', 1);
+    score_map.insert(']', 2);
+    score_map.insert('}', 3);
+    score_map.insert('>', 4);
+
+    let incomplete_lines = lines
+        .map(|l| l.unwrap())
+        .filter(|l| is_corrupt(&l).is_none());
+    let mut scores: Vec<usize> = incomplete_lines
+        .map(|l| completion_score(completion(&l), &score_map))
+        .collect();
+    scores.sort();
+    return scores[scores.len() / 2];
 }
--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use std::fmt::Debug;
 use std::io::BufRead;
 use std::{fs::File, io, path::Path};
 
-use crate::day10::day10part1;
+use crate::day10::{day10part1, day10part2};
 use crate::day3::day3part2;
 use crate::day4::{day4part1, day4part2};
 use crate::day5::day5part1;
@@ -39,7 +39,8 @@ fn main() {
     // println!("day8part2 {:?}", day8part2());
     // println!("day9part1 {:?}", day9part1());
     // println!("day9part2 {:?}", day9part2());
-    println!("day10part1 {:?}", day10part1());
+    // println!("day10part1 {:?}", day10part1());
+    println!("day10part2 {:?}", day10part2());
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
