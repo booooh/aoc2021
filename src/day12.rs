93326f0c0c85e0f6df87e549b2beeb52f8dccff4
booooh <ben.dayan@gmail.com>
2021-12-19 17:02:01 +0200

    day12 part1

--- src/day12.rs
+++ src/day12.rs
@@ -4,6 +4,7 @@ use std::{
     fs::File,
     io::{BufRead, BufReader, Lines, Result},
     path::Path,
+    thread::current,
 };
 
 fn parse_line(s: &str) -> (String, String) {
@@ -50,13 +51,79 @@ impl Graph {
 
         return graph;
     }
+
+    // fn backtrack(&self, mut path: Vec<String>) -> (Vec<String>, String) {
+    //     // 1. remove the latest item in the path
+    //     let curr_last = path.pop().unwrap();
+
+    //     // take a new sibling for the last element
+    //     let prev_last = path.last().unwrap();
+    //     let possible_neighbors = &self.edges[prev_last];
+    //     let new_index = possible_neighbors
+    //         .iter()
+    //         .position(|x| x == &curr_last)
+    //         .unwrap()
+    //         + 1;
+    //     if new_index == possible_neighbors.len() {
+    //         // this was the last sibling, remove it
+    //     }
+    // }
+
+    fn all_paths(&self, start: String, end: String) -> HashSet<Vec<String>> {
+        let mut visited = Vec::<String>::new();
+        visited.push(start);
+        let mut paths = HashSet::<Vec<String>>::new();
+        let mut next_index = 0;
+
+        // assumes there are no loops - so just add to the path if it's possible
+        loop {
+            // try to extend the path, find the next sibling after the last one used
+            let current = visited.last().unwrap().to_owned();
+            let possible_extensions = self.edges.get(&current).unwrap();
+            if possible_extensions.len() > next_index {
+                // check if valid:
+                let ext = &possible_extensions[next_index];
+                if &ext.to_uppercase() == ext || !visited.contains(ext) {
+                    visited.push(ext.to_string());
+                    next_index = 0;
+                } else {
+                    next_index += 1;
+                }
+            } else {
+                // if this is a good path, record it
+                if current == end {
+                    // println!("found end, going to save the path");
+                    paths.insert(visited.clone());
+                }
+
+                //now backtrack, if possible - if not - return:
+                let dropped_node = visited.pop().unwrap();
+                if visited.len() == 0 {
+                    return paths;
+                }
+
+                // index of the next element
+                let last_node = visited.last().unwrap();
+                next_index = self
+                    .edges
+                    .get(last_node)
+                    .unwrap()
+                    .iter()
+                    .position(|x| x == &dropped_node)
+                    .unwrap()
+                    + 1;
+            }
+        }
+    }
 }
 
 pub(crate) fn day12part1() -> usize {
     let mut lines = read_lines("input12.t").unwrap();
     let g = Graph::from_lines(lines);
     println!("{:?}", g);
-    return 0;
+    let paths = g.all_paths("start".to_string(), "end".to_string());
+    println!("{:?}", paths);
+    return paths.len();
 }
 
 pub(crate) fn day12part2() -> usize {
